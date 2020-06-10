156fe1fa9f47e9637a0f26461a78c9aaf6ce13d2
Mihail Karlovski <mihailkarlovski@gmail.com>
2020-06-10 18:07:41 +0200

    Update UserModifyModel.cs

--- MyMovies/MyMovies/ViewModels/UserModifyModel.cs
+++ MyMovies/MyMovies/ViewModels/UserModifyModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MyMovies.Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,13 @@ namespace MyMovies.ViewModels
 {
     public class UserModifyModel
     {
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public bool IsAdmin { get; set; }
+        public List<MovieComment> MovieComments { get; set; }
     }
 }
